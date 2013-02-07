1e8571c477bd57dbd4002fe48b6bcfdb299e3a15
Steve Gattuso <stevenleeg@gmail.com>
2013-02-06 21:21:16 -0500

    Minor bugfix and setting up package for ppi

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name="Minecraft Log Parser",
+    version="1.0",
+    description="A simple parser of Minecraft server logs",
+    author="Steve Gattuso",
+    author_email="steve@stevegattuso.me",
+    url="http://github.com/stevenleeg/Minecraft-Log-Parser",
+    scripts=["bin/mclp"]
+)
