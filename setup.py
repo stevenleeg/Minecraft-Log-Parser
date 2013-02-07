d2a10ed99f7e7be7e7e966304156c8973d019ac5
Steve Gattuso <stevenleeg@gmail.com>
2013-02-06 21:25:57 -0500

    A few modifications to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@
 from distutils.core import setup
 
 setup(
-    name="Minecraft Log Parser",
+    name="mclp",
     version="1.0",
     description="A simple parser of Minecraft server logs",
+    keywords="minecraft log parser",
     author="Steve Gattuso",
+    license="MIT",
     author_email="steve@stevegattuso.me",
     url="http://github.com/stevenleeg/Minecraft-Log-Parser",
     scripts=["bin/mclp"]
